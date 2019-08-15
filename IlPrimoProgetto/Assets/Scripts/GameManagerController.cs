43b68716d4a60510c9ef39eac9ed4701218fb2d1
craken-dev <craken.dev@gmail.com>
2019-08-15 15:55:39 +0200

    aggiunta meccanica di gioco base
    Se si preme un qualsiasi punto dello schermo il tempo viene rallentato; quando si rilascia lo schermo viene applicata una forza opposta alla direzione del bersaglio (rinculo)

---
+++ IlPrimoProgetto/Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour
+{
+    public static GameManagerController obj;
+
+    [SerializeField] private float slow_time_factor = 0.04f;
+    private void Awake()
+    {
+        if(obj == null)
+        {
+            obj = this;
+        }
+        else if(obj != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SlowTime()
+    {
+        Time.timeScale = slow_time_factor;
+        Time.fixedDeltaTime = 1/50 * slow_time_factor;
+    }
+
+    public void NormalTime()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = 1 / 50;
+    }
+
+
+}
---
+++ IlPrimoProgetto/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovementController : MonoBehaviour
+{
+
+    [SerializeField] private float shooting_force = 10f;
+
+    Touch input;
+    private bool aming = false;
+    private bool shooting = false;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            input = Input.GetTouch(0);
+            switch (input.phase)
+            {
+                case TouchPhase.Began:
+                    aming = true;
+                    break;
+                case TouchPhase.Ended:
+                    shooting = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (aming)
+        {
+            Vector2 aming_target = Camera.main.ScreenToWorldPoint((Vector2) input.position);
+
+            GameManagerController.obj.SlowTime();
+
+            if (shooting)
+            {
+                Vector2 aming_vector = aming_target - (Vector2)transform.position;
+                aming_vector.Normalize();
+                aming_vector *= shooting_force;
+                rb.velocity = Vector2.zero;
+                rb.velocity = -aming_vector;
+
+                GameManagerController.obj.NormalTime();
+
+                aming = false;
+                shooting = false;
+            }
+        }
+    }
+}
