f668566a5292189294139ce48e31c29f5c7d830a
craken-dev <craken.dev@gmail.com>
2019-08-17 15:06:06 +0200

    ...

--- IlPrimoProgetto/Assets/Scripts/GameManagerController.cs
+++ IlPrimoProgetto/Assets/Scripts/GameManagerController.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class GameManagerController : MonoBehaviour
 {
     public static GameManagerController obj;
-
-    [SerializeField] private float slow_time_factor = 0.04f;
-    private bool is_time_slowed = false;
     private void Awake()
     {
         if(obj == null)
@@ -19,26 +16,5 @@ public class GameManagerController : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    public void SlowTime()
-    {
-        if (!is_time_slowed)
-        {
-            Time.timeScale = slow_time_factor;
-            Time.fixedDeltaTime = 1 / 50 * slow_time_factor;
-            is_time_slowed = true;
-        }
-    }
-
-    public void NormalTime()
-    {
-        if (is_time_slowed)
-        {
-            Time.timeScale = 1;
-            Time.fixedDeltaTime = 1 / 50;
-            is_time_slowed = false;
-        }
-    }
-
 
 }
